e9ae0d7854ff55f52a1f1b5496efd164492e63a9
Knut Haugen <knut.haugen@nrk.no>
2018-12-19 12:16:42 +0100

    QA on insertall

--- insertAll.sh
+++ insertAll.sh
@@ -5,6 +5,11 @@ node import/importer.js -a -f ~/Dropbox/privat/sykkel/enduro-resultater/2012/kon
 node import/importer.js -a -f ~/Dropbox/privat/sykkel/enduro-resultater/2013/nesbyen/nesbyen-2013.csv #ok!
 node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2013/oslo/oslo-2013.csv # ok!
 node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2014/drammen/drammen-2014.csv # ok
+node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2014/oslo/oslo-2014.csv # ok
+node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2014/traktor/traktor-2014.csv # ok
+node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2014/nesbyen/nesbyen-2014.csv # ok
+node import/importer.js -f ~/Dropbox/privat/sykkel/enduro-resultater/2014/sogndal/sogndal-2014.csv # ok
+
 #node import/importer.js -d ~/Dropbox/privat/sykkel/enduro-resultater/2015/luster/ #not ok
 #node import/importer.js -a -d ~/Dropbox/privat/sykkel/enduro-resultater/2015/nesbyen #ok!
 #node import/importer.js -a -d ~/Dropbox/privat/sykkel/enduro-resultater/2015/oppdal/ #ok!
