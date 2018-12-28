efd15952c35531c23828231e74ac564835ea7223
Knut Haugen <knuthaug+github@gmail.com>
2018-12-28 16:47:28 +0100

    hased asset also for sort-table css

--- server/app.js
+++ server/app.js
@@ -149,7 +149,7 @@ app.get('/assets/css/:file', (req, res) => {
   const file = req.params.file
   const options = { root: './server/dist' }
 
-  if(/bundle/.test(file)) {
+  if(/bundle/.test(file) || /sort/.test(file)) {
     return res.set({'Cache-Control': 'public, max-age=100000'}).sendFile(`css/${file}`, options)    
   }
   return res.set({'Cache-Control': 'public, max-age=1000'}).sendFile(`css/${file}`, options)
