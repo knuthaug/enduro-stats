68f2ebe30bae8c46e2c4354f734c8a2e71e482fa
Knut Haugen <knuthaug+github@gmail.com>
2019-05-16 20:57:36 +0200

    sportident json parsing WIP

---
+++ import/converters/sportident_json.js
@@ -0,0 +1,133 @@
+/**
+ * @fileOverview Sportiden json race format parser. it requires the following fields/conventions
+ to work properly:
+ * @name sportident_json.js
+ * @author Knut Haugen
+ * @license ISC
+ */
+
+const fs = require('await-fs')
+const logger = require('../logger.js')
+const { ERROR_RANK, DNS_STATUS, DNF_STATUS, OK_STATUS } = require('../constants.js')
+const { check } = require('../spellcheck.js')
+const { convertTimeToMs } = require('../../lib/time.js')
+const Converter = require('./converter.js')
+const lib = require('./lib.js')
+class SportidentJson extends Converter {
+  /**
+   * A parser/converter for mylaps race results, with multiple stages on one line
+   * @constructor
+   * @param {string} filename - filename containing results
+   * @param {string} options - options object. Only key 'datafile' is supported, full path to json file with extra race data not in the results file
+   */
+  constructor (filename, options) {
+    super()
+    this.filename = filename
+    this.options = options || { }
+
+    if (this.options.hasOwnProperty('datafile')) {
+      this.datafileName = options.datafile
+    }
+  }
+
+  /**
+   * Load data files for this converter, stored in this
+   */
+  async load () {
+    const stats = await fs.stat(this.filename)
+
+    if (stats.isFile()) {
+      logger.info(`reading file ${this.filename}`)
+      this.file = await fs.readFile(this.filename, 'utf-8')
+
+      if (this.datafileName) {
+        const stats = await fs.stat(this.datafileName)
+
+        if (stats.isFile()) {
+          logger.info(`reading data file ${this.datafileName} for extra data`)
+          this.datafile = await fs.readFile(this.datafileName, 'utf-8')
+        } else {
+          logger.error(`Data file ${this.datafileName} was not found`)
+        }
+      }
+      return this
+    }
+
+    logger.error(`File ${this.filename} was not found`)
+    throw new Error(`file ${this.filename} does not exist`)
+  }
+
+  async parse () {
+    let race = {}
+
+    if (this.datafile) {
+      try {
+        race = JSON.parse(this.datafile)
+      } catch (err) {
+        console.log(err)
+        process.exit(1)
+      }
+    }
+
+    race.uid = this.checksum(race.name + race.year)
+    const stages = await this.parseStages()
+    return { race, stages }
+  }
+
+  /**
+   * parse stages and return array of results, flattened out ready for database storage.
+   * @return { race, stages } - a race object and a list of stages, with results per stage
+   */
+  async parseStages () {
+    let raw
+    try {
+      raw = JSON.parse(this.file)
+    } catch (error) {
+      logger.error(error)
+      process.exit(1)
+    }
+    const stages = this.findStages(raw)
+    return stages
+  }
+
+  findGender(clazz) {
+    if(/kvinner/i.test(clazz) || /^k/i.test(clazz)) {
+      return 'F'
+    }
+    return 'M'
+  }
+
+  convertTime (time, pos) {
+    if (lib.finished(pos)) {
+      return time
+    }
+
+    return '00:00:00'
+  }
+
+  setStatus (pos, time) {
+    if (pos === 0 || pos === '0') {
+      return DNS_STATUS
+    } else if (time === '00:00:00') {
+      return DNS_STATUS
+    } else if (lib.notFinished(pos)) {
+      return pos
+    } else if (lib.finished(pos)) {
+      return 'OK'
+    }
+    return pos
+  }
+
+  findStages(raw) {
+    let number = 1
+    const entry = raw.results[0]
+    return entry.stages.map((e) => {
+      return {
+        name: e.name,
+        number: number++
+      }
+    })
+  }
+}
+
+module.exports = SportidentJson
---
+++ test/sportident_json_test.js
@@ -0,0 +1,68 @@
+const tap = require('tap')
+const SportidentJson = require('../import/converters/sportident_json.js')
+const path = require('path')
+
+tap.test('parse complete result file', async t => {
+  const ml = new SportidentJson(path.join(__dirname, 'data/hakadal.json'), {
+    datafile: path.join(__dirname, 'data/sportident-racedata.json')
+  })
+
+  const loaded = await ml.load()
+  const data = await loaded.parse()
+  t.equals(data.race.name, 'Hakadal enduro snowhill', 'race name matches')
+  t.equals(data.race.year, 2019, 'race year matches')
+  t.equals(data.race.date, '2019-03-02', 'race date matches')
+  t.equals(data.race.uid, 'fc51d6c0a4bb7ff71ba81a05c12f632a', 'race uid matches')
+})
+
+tap.test('Object details for stages', async t => {
+  const ml = new SportidentJson(path.join(__dirname, 'data/hakadal.json'), {
+    datafile: path.join(__dirname, 'data/sportident-racedata.json')
+  })
+
+  const loaded = await ml.load()
+  const data = await loaded.parse()
+  t.equals(data.stages.length, 4, 'correct number of stages')
+  const stage1 = data.stages[0]
+  const stage2 = data.stages[1]
+
+  t.equals(stage1.name, 'FE 1', 'stage name matches')
+  t.equals(stage1.number, 1, 'stage number matches')
+  t.equals(stage2.name, 'FE 2', 'stage name matches')
+  t.equals(stage2.number, 2, 'stage number matches')
+  t.equals(stage1.results.length, 41, 'stage result length matches')
+  const rider = stage1.results[0]
+
+  t.equals(rider.name, 'Anna Littorin-Sandbu', 'name is correct')
+  t.equals(rider.rider_uid, '4e568767810427927204e9f66da1171d', 'uid is correct')
+  t.equals(rider.time, '00:01:14', 'time is correct')
+  t.equals(rider.gender, 'F', 'Gender is correct')
+  t.equals(rider.class, 'Kvinner senior', 'class is correct')
+  t.equals(rider.club, 'Lillehammer Ck', 'club is correct')
+  t.equals(rider.stage_time_ms, 74000, 'time in ms is correct')
+  t.equals(rider.acc_time_ms, null, 'acc_time_ms is null')
+  t.equals(rider.stage_rank, 2, 'rank is correct')
+  t.equals(rider.status, 'OK', 'status is correct')
+
+  t.equals(stage1.results[1].stage_rank, 1)
+
+  t.equals(stage1.results[2].time, '00:01:37')
+  t.equals(stage1.results[2].rider_uid, '0e6a47137d050ee9bd05f784853b4038', 'uid is correct')
+  t.equals(stage1.results[2].stage_rank, 3)
+  t.equals(stage1.results[2].stage_time_ms, 97000)
+  t.equals(stage1.results[2].status, 'OK')
+})
+
+tap.test('final status field is correct', async t => {
+  const ml = new SportidentJson(path.join(__dirname, 'data/hakadal.json'), {
+    datafile: path.join(__dirname, 'data/sportident-racedata.json')
+  })
+
+  const loaded = await ml.load()
+  const data = await loaded.parse()
+
+  const lastStage = data.stages[3]
+  t.equals(lastStage.results[0].final_status, 'OK', 'winner has OK final_status')
+  //  console.log(lastStage.results[lastStage.results.length - 17])
+  t.end()
+})
