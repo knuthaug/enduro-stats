de94ed21c09b400a4534d9c278f0e5039dcca68d
Knut Haugen <knut.haugen@nrk.no>
2019-09-16 09:33:09 +0200

    add nesbyen 2019

--- insertAll.sh
+++ insertAll.sh
@@ -171,3 +171,6 @@ node import/importer.js -f results/2019/harstad/harstad-2019.csv
 
 echo "  Hakadal"
 node import/importer.js -s -f results/2019/hakadal/hakadal-2019.csv
+
+echo "  Nesbyen"
+node import/importer.js -m -f results/2019/nesbyen/nesbyen-2019.csv
