ff9a9087de8618667f3dbae9fdc258e000d2d5f5
Knut Haugen <knut.haugen@nrk.no>
2019-10-05 20:35:45 +0200

    add table to delete_tables script

--- scripts/delete_tables.sql
+++ scripts/delete_tables.sql
@@ -2,6 +2,7 @@ drop table insert_log;
 drop table migrations;
 drop table results;
 drop table race_links;
+drop table stages_details;
 drop table rider_races;
 drop table rider_rankings;
 drop table riders;
