c1c786d7a930b0266858291de09c8b4f5b982bd5
Knut Haugen <knut.haugen@nrk.no>
2019-11-08 19:39:56 +0100

    added images for hakadal and oslo

--- scripts/match-images.js
+++ scripts/match-images.js
@@ -5,20 +5,62 @@ const Db = require('../import/db.js')
 var fs = require('fs');
 const db = new Db()
 
+const bylineMap = {
+  'Jon Borgersen': {
+    text: 'Jon Borgersen',
+    url: 'http://jonborgersen.com/'
+  },
+  'Pål Westgaard': {
+    text: 'Pål Westgaard/westgaardfoto',
+    url: 'http://westgaardfoto.no/'
+  },
+  'Moldestad Photography': {
+    text: 'Moldestad Photography',
+    url: 'https://www.facebook.com/moldestadphotography/'
+  },
+  'Eirik Grønevik': {
+    text: 'Eirik Grønevik/mtbfoto.no',
+    url: 'http://mtbfoto.no/'
+  },
+  'Kristoffer H. Kippernes': {
+    text: 'Kristoffer H. Kippernes/Terrengsykkel',
+    url: 'http://terrengsykkel.no/'
+  },
+  'Ola Morken': {
+    text: 'Ola Morken',
+    url: ''
+  }
+
+}
+
 init();
 
 async function init() {
   const optionDefinitions = [
     { name: 'race', alias: 'r', type: String, defaultOption: true },
-    { name: 'byline', alias: 'b', type: String },
-    { name: 'url', alias: 'u', type: String }
   ]
 
   const options = cmd(optionDefinitions)
-  startWithList(options.race, options)
+  startWithByline(options.race, options)
 }
 
-async function startWithList(raceId, options) {
+async function startWithByline(race, options) {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'byline',
+        message: 'Velg byline',
+        choices: Object.keys(bylineMap)
+      }
+    ])
+    .then(async answers => {
+      startWithList(race, options, bylineMap[answers.byline])
+    })
+
+}
+
+async function startWithList(raceId, options, byline) {
   const riders = await getRiders(raceId)
   inquirer
     .prompt([
@@ -30,30 +72,35 @@ async function startWithList(raceId, options) {
       }
     ])
     .then(async answers => {
-      //console.log(JSON.stringify(answers, null, '  '));
-      const parts = answers.rider.split(' ')
-      const uid = parts[5]
-      const bib = parts[0]
-      const riderId = parts[4]
-      //mv image to images/uid.jpg
-
-      fs.rename(`images/${bib}.jpg`, `images/${uid}.jpg`, function(err) {
-        if ( err ) {
-          console.log('ERROR: ' + err);
-        }
-        console.log(`renamed image images/${bib}.jpg to images/${uid}.jpg`)
-      });
-      //update riders table with byline
-      //await db.addByline(riderId, options.byline, options.url)
-      console.log(`added byline info to rider ${riderId}`)
-      //add to file
-      fs.appendFile('scripts/byline.sql', `UPDATE riders set byline_text='${options.byline}', byline_url='${options.url}' where id=${riderId};\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-      //restart
-      startWithList(raceId)
+      await matchImage(answers, byline)
+      startWithList(raceId, options, byline)
     });
+}
+
+async function matchImage(answers, byline) {
+  //console.log(JSON.stringify(answers, null, '  '));
+  const parts = answers.rider.split(';')
+  const uid = parts[3]
+  const bib = parts[0]
+  const riderId = parts[2]
+  //mv image to images/uid.jpg
+
+  fs.rename(`raw_images/${bib}.jpg`, `images/${uid}.jpg`, function(err) {
+    if ( err ) {
+      console.log('ERROR: ' + err);
+      return
+    }
+    console.log(`renamed image images/${bib}.jpg to images/${uid}.jpg`)
+  });
+  //update riders table with byline
+  await db.addByline(riderId, byline.text, byline.url)
+  console.log(`added byline info to rider ${riderId}`)
+  //add to file
+  fs.appendFile('scripts/byline.sql', `UPDATE riders set byline_text='${byline.text}', byline_url='${byline.url}' where id=${riderId};\n`, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  //restart
 }
 
 async function getRiders(id) {
@@ -68,6 +115,6 @@ async function getRiders(id) {
   }).filter((r) => {
     return r.bib !== ''
   }).map((r) => {
-    return `${r.bib} ${r.name} ${r.id} ${r.uid}`
+    return `${r.bib};${r.name};${r.id};${r.uid}`
   })
 }
