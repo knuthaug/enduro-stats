7b61b256f8e6702338081b11010a4f6d1ba9e54c
Knut Haugen <knut.haugen@nrk.no>
2020-01-02 20:32:57 +0100

    no logfile for test env

--- import/logger.js
+++ import/logger.js
@@ -1,9 +1,17 @@
 const pino = require('pino')
 const config = require('../config')
 
-const logger = pino(pino.destination('./logs/import.log'), {
-  name: 'enduro-stats-import',
-  level: config.get('import.loglevel')
-})
+let logger
 
+if(config.get('env') !== 'test') {
+  logger = pino(pino.destination('./logs/import.log'), {
+    name: 'enduro-stats-import',
+    level: config.get('import.loglevel')
+  })
+} else {
+  logger = pino({
+    name: 'enduro-stats-import',
+    level: config.get('import.loglevel')
+  })
+}
 module.exports = logger
