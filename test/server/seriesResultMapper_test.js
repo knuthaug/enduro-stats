e71377acfb6b07535477d82ddf07e52abfd9bceb
Knut Haugen <knut.haugen@nrk.no>
2020-02-16 20:06:48 +0100

    WIP add tests for seriesTotal mapping

---
+++ test/server/seriesResultMapper_test.js
@@ -0,0 +1,16 @@
+const tap = require('tap')
+const fs = require('fs')
+const path = require('path')
+const deepFreeze = require('deep-freeze')
+
+const mapper = require('../../server/seriesResultMapper.js')
+const data = deepFreeze(JSON.parse(fs.readFileSync(path.join(__dirname, '../data/racesForSeries.json')).toString()))
+
+const r = mapper(data)
+
+tap.test('maps to classes', (t) => {
+  t.equals(r.length, 10, 'all classes in toplevel')
+  t.end()
+})
+
+
