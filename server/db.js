72e2af491877c5785dc6f4f93ebe4691a4b2f299
Knut Haugen <knut.haugen@nrk.no>
2020-02-18 14:50:37 +0100

    fix for series name on beito results

--- server/db.js
+++ server/db.js
@@ -164,9 +164,9 @@ class Db {
   }
 
   async racesBySeriesAndYear(series, year) {
-    const query = 'select id, race_id, (select uid from riders where id = rider_id) as uid, (select name from races where race_id = id) as race_name, (select date from races where race_id = id) as race_date, class, final_rank, acc_time_ms, acc_time_behind, (SELECT name FROM riders where id = results.rider_id) as name from results where race_id in (select id from races where series = $1 and year = $2) AND final_rank IS NOT NULL AND class NOT ILIKE $3 order by race_date, class, final_rank'
+    const query = 'select id, race_id, (select uid from riders where id = rider_id) as uid, (select name from races where race_id = id) as race_name, (select date from races where race_id = id) as race_date, class, final_rank, acc_time_ms, acc_time_behind, (SELECT name FROM riders where id = results.rider_id) as name from results where race_id in (select id from races where series = $1 and year = $2) AND final_rank IS NOT NULL AND class NOT ILIKE $3 AND class NOT ILIKE $4 order by race_date, class, final_rank'
 
-    const values = [series, year, '%funduro%']
+    const values = [series, year, '%funduro%', '%explorer%']
     return this.find(query, values)
   }
 
