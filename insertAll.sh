869bb9b20c477c9bf185b114873b00766d92992f
Knut Haugen <knuthaug+github@gmail.com>
2021-09-05 21:26:47 +0200

    nesbyen

--- insertAll.sh
+++ insertAll.sh
@@ -193,4 +193,7 @@ node import/importer.js -m -f results/2021/traktorland/traktorland-2021.csv
 echo "  Harstad"
 node import/importer.js -f results/2021/harstad/harstad-2021.csv
 
+echo "  Nesbyen"
+node import/importer.js -m -f results/2021/nesbyen/nesbyen-2021.csv
+
 psql -U endurostats endurostats < scripts/byline.sql
