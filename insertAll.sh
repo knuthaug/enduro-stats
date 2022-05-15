47c8ff03c26b72692ab32f262036afe3253bf120
Knut Haugen <knuthaug+github@gmail.com>
2022-05-15 12:18:22 +0200

    telemark 2022

--- insertAll.sh
+++ insertAll.sh
@@ -207,4 +207,10 @@ node import/importer.js -a -f results/2021/spikkestad/spikkestad-2021.csv
 echo "  Trysil Enduro"
 node import/importer.js -m -f results/2021/trysil/trysil-2021.csv
 
+
+echo "2022"
+echo "  Telemark Enduro"
+node import/importer.js -m -f results/2022/telemark/telemark-2022.csv
+
 psql -U endurostats endurostats < scripts/byline.sql
+
