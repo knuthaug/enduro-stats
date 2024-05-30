f5ef76eb6c77ca51ccc9404d1b1f75c3ef0639d9
Knut Haugen <knut.haugen@nrk.no>
2024-05-30 07:35:15 +0200

    Add mistberget 2024 results

--- import/converters/converter.js
+++ import/converters/converter.js
@@ -5,51 +5,63 @@
  * @license ISC
  */
 
-const md5 = require('md5')
-const { checkClub } = require('../spellcheck.js')
+const md5 = require("md5");
+const { checkClub } = require("../spellcheck.js");
 
 class Converter {
-  raceChecksum (obj) {
-    return this.checksum(this.raceName(obj) + this.year(obj))
+  raceChecksum(obj) {
+    return this.checksum(this.raceName(obj) + this.year(obj));
   }
 
-  checksum (string) {
-    return md5(string)
+  checksum(string) {
+    return md5(string);
   }
 
-  clubName (name) {
-    return checkClub(name)
+  clubName(name) {
+    return checkClub(name);
   }
 
-  className (name) {
+  className(name) {
     if (/Master menn|menn master|Master herrer/i.test(name)) {
-      return 'Menn master'
+      return "Menn master";
     } else if (/master kvinner|kvinner master/i.test(name)) {
-      return 'Kvinner master'
-    } else if (/Kvinner Åpen.*17+/i.test(name) || /Menn Åpen.*17+|Menn trim/i.test(name) || /.*Elduro.*/i.test(name) || /M\s?17-18/.test(name) || /Kvinner U23/.test(name) || /.* Åpen.*?el.*?/i.test(name) || /Åpen klasse .*/i.test(name)) {
-      return name
-    } else if (/K\s?17+/.test(name)) {
-      return 'Kvinner senior'
+      return "Kvinner master";
+    } else if (
+      /Kvinner Åpen.*17+/i.test(name) ||
+      /Menn Åpen.*17+|Menn trim/i.test(name) ||
+      /.*Elduro.*/i.test(name) ||
+      /M\s?17-18/.test(name) ||
+      /Kvinner U23/.test(name) ||
+      /.* Åpen.*?el.*?/i.test(name) ||
+      /Åpen klasse .*/i.test(name)
+    ) {
+      return name;
+    } else if (/K\s?17\+/.test(name)) {
+      return "Kvinner senior";
     } else if (/M\s?17\+/.test(name)) {
-      return 'Menn senior'
-    } else if (/Funduro/i.test(name) || /Explorer/i.test(name) ||
-               /M\s?\d\d|K\s?\d\d/i.test(name) || /Menn \d\d|Kvinner \d\d/i.test(name)) {
-      return name
+      return "Menn senior";
+    } else if (
+      /Funduro/i.test(name) ||
+      /Explorer/i.test(name) ||
+      /M\s?\d\d|K\s?\d\d/i.test(name) ||
+      /Menn \d\d|Kvinner \d\d/i.test(name)
+    ) {
+      return name;
     } else if (/M.+junior|m.+jr/i.test(name)) {
-      return 'Menn junior'
+      return "Menn junior";
     } else if (/K.+junior|k.+jr/i.test(name)) {
-      return 'Kvinner junior'
+      return "Kvinner junior";
     } else if (/^Menn sport/i.test(name)) {
-      return 'Menn sport'
+      return "Menn sport";
     } else if (/^M|Menn/i.test(name)) {
-      return 'Menn senior'
+      return "Menn senior";
     } else if (/^K|Kvinner/i.test(name) || /Women/i.test(name)) {
-      return 'Kvinner senior'
+      return "Kvinner senior";
     } else if (/^\d/.test(name)) {
-      return name.replace(/^\d\s?/, '')
+      return name.replace(/^\d\s?/, "");
     }
-    return name
+    return name;
   }
 }
 
-module.exports = Converter
+module.exports = Converter;
--- insertAll.sh
+++ insertAll.sh
@@ -264,5 +264,10 @@ node import/importer.js -a -f results/2023/lillehammer/lillehammer-2023.csv
 echo "  Trysil"
 node import/importer.js -m -f results/2023/trysil/trysil-2023.csv
 
+echo "2024"
+
+echo "  Mistberget"
+node import/importer.js -m -f results/2024/mistberget/mistberget-2024.csv
+
 psql -U endurostats endurostats < scripts/byline.sql
 
