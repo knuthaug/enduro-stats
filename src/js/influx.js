66034a2b6bcda5cffce7e7e59a6cc45a59a8b325
Knut Haugen <knut.haugen@nrk.no>
2025-02-22 19:40:35 +0100

    add hits

--- src/js/influx.js
+++ src/js/influx.js
@@ -94,7 +94,6 @@ async function writeVitals(vitals, page = document.location.pathname) {
       },
     });
     const body = await res.json();
-    console.log("message", body.message);
   } catch (err) {
     console.error(err);
   }
@@ -111,7 +110,7 @@ async function writeINP(inp, page = document.location.pathname) {
       body: JSON.stringify({
         inp,
         page: page,
-        domain: "blog.knuthaugen.no",
+        domain: "norskenduro.no",
         browser: browser?.name,
         browserVersion: browser?.version,
         os: browser?.os,
@@ -122,7 +121,32 @@ async function writeINP(inp, page = document.location.pathname) {
       },
     });
     const body = await res.json();
-    console.log("message", body.message);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function writeHit(page = document.location.pathname) {
+  const browser = detect();
+
+  try {
+    const res = await fetch(`${baseURL}/hit`, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        count: 1,
+        page: page,
+        domain: "norskenduro.no",
+        browser: browser?.name,
+        browserVersion: browser?.version,
+        os: browser?.os,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await res.json();
+    console.log("hit message", body.message);
   } catch (err) {
     console.error(err);
   }
